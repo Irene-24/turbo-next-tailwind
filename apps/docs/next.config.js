fe2598256d10d9413ec339ec6b8af926d5d54fec
Irene-24 <okaforirenen@gmail.com>
2023-01-06 07:55:22 +0100

    fix stuff

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
-
+  output: "standalone",
+  
   experimental: {
     transpilePackages: ["ui", "redux-utils", "hooks-and-utils"],
   },
